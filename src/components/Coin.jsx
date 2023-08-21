4cb50ed3257df8142b800cc6860fed1715d572b8
ondrejvosmera <ondravosmera@seznam.cz>
2023-08-21 17:03:19 +0200

    main_done

--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const Coin = ({rank, image, name, symbol, change, price, mcap}) => {
+const Coin = ({rank, image, name, symbol, price, change, price_change, mcap}) => {
   return (
-    <div className="coin-row">
-        <p>{rank}</p>
-        <img src={image} alt={name} />
-        <p>{name}{symbol}</p>
-        <p>{price}</p>
-        <p>{change}</p>
-        <p>{mcap}</p>
+    <Link to={`/${name}`} key={name}>
+    <div className="coin-cell">
+      <div className="col1"><p>{rank}.</p></div>
+      <div className="col2"><img src={image} alt={name} /><p className='coin-name'>{name}<span className='coin-symbol'>{symbol}</span></p></div>
+        <div className="col3"><p>${price.toFixed(2)}</p></div>
+        <div className={`col4 coin-positive-negative ${change < 0 ? 'negative' : 'positive'}`}><p>{change.toFixed(2)}%</p></div>
+        <div className={`col5 coin-positive-negative ${change < 0 ? 'negative' : 'positive'}`}><p>{price_change.toLocaleString()}</p></div>
+      <div className="col6"><p>${mcap.toLocaleString()}</p></div>
     </div>
+    </Link>
   )
 }
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { CoinList } from '../config/api.js';
 import Coin from './Coin.jsx';
+import { AiOutlineSearch } from 'react-icons/ai';
 
 
 
@@ -34,32 +35,34 @@ const Main = () => {
   return (
     <div className="main-container">
         <div className="search">
-            <h2>Search a coin</h2>
+            <h2>Search</h2>
             <form>
                 <input type="text" 
-                placeholder='Search' 
+                placeholder={AiOutlineSearch} 
                 className='coin-input' 
                 onChange={(e) => setSearch (e.target.value)} />
             </form>
         </div>
         <div className="coin-list">
             <div className="coin-header">
-                <p>#</p>
-                <p>Coin</p>
-                <p>Price</p>
-                <p>24h %</p>
-                <p>24h <span>Volume</span></p>
-                <p>Market cap</p>
+                <div className="col1"><p>#</p></div>
+                <div className="col2"><p>Coin</p></div>
+                <div className="col3"><p>Price</p></div>
+                <div className="col4"><p>24h %</p></div>
+                <div className="col5"><p>24h change</p></div>
+                <div className="col6"><p>Market cap</p></div>
             </div>
             <div className="coin-row">
                 {filteredCoins.map(coin =>{
                     return (
                         <Coin 
                             key={coin.id}
+                            rank={coin.market_cap_rank}
                             image={coin.image}
                             name={coin.name}
                             symbol={coin.symbol}
                             change={coin.price_change_percentage_24h}
+                            price_change={coin.price_change_24h}
                             price={coin.current_price}
                             mcap={coin.market_cap}
                         />
